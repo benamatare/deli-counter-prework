f40a708d58540feec2e6e8a0abcd5e846858bf6a
Ben Matare <ben.a.matare@gmail.com>
2018-01-22 00:51:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+
+
+def line(deli)
+  current_line = "The line is currently:"
+  if deli.size == 0
+    puts "The line is currently empty."
+  else
+    counter = 0
+    deli.each {|name| current_line << "#{counter}. #{deli[counter-1]}" counter =+ 1}
+    puts current_line
+  end
+end
