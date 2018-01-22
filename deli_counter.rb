48afa53e65d638c5c70792d22fe6ecb89964c27f
Ben Matare <ben.a.matare@gmail.com>
2018-01-22 00:52:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(deli)
     puts "The line is currently empty."
   else
     counter = 0
-    deli.each {|name| current_line << "#{counter}. #{deli[counter-1]}" counter =+ 1}
+    deli.each {|name| current_line << "#{counter}. #{deli[counter-1]}" counter +=1}
     puts current_line
   end
 end
